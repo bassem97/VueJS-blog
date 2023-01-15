9b091445fa63d47db0b86e19021c093aee9d32bd
BASSEM <bassem.jadoui@esprit.tn>
2023-01-15 12:20:01 +0100

    injecting type script via vue CLI

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
-import { createApp, h } from 'vue'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from '@/router'
 
-const app = createApp({
-  render: () => h(App)
-})
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
